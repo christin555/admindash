4061ca7ef769a4e99ee359325b83e52ce1778cb0
Крис <kriska_555@mail.ru>
2023-02-11 13:40:01 +0500

    release 11/02

--- src/api/index.js
+++ src/api/index.js
@@ -2,34 +2,34 @@ import axios from 'axios';
 
 const API_URL = `http://localhost:8800/admin/`;
 const baseQuery = axios.create({
-    baseURL: API_URL,
-    responseType: 'json',
-    // withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        authorization: `Bearer ${localStorage.token}`
-    }
+  baseURL: API_URL,
+  responseType: 'json',
+  // withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    authorization: `Bearer ${localStorage.token}`
+  }
 });
 
 const verify = (err) => {
-    if (err.response?.status === 401) {
-        localStorage.removeItem("token");
-        window.location.reload();
-    }
+  if (err.response?.status === 401) {
+    localStorage.removeItem('token');
+    window.location.reload();
+  }
 
-    throw new Error(err)
-}
+  throw new Error(err);
+};
 
 const API = {
-    post: (address, body) => baseQuery
-        .post(address, body)
-        .then(({data}) => data)
-        .catch((err) => verify(err)),
-    get: (address) => baseQuery
-        .get(address)
-        .then(({data}) => data)
-        .catch((err) => verify(err)),
+  post: (address, body) => baseQuery
+    .post(address, body)
+    .then(({data}) => data)
+    .catch((err) => verify(err)),
+  get: (address) => baseQuery
+    .get(address)
+    .then(({data}) => data)
+    .catch((err) => verify(err))
 };
 
 export default API;
