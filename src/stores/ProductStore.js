8502de9b8073798a2f94cfafb0d20961de397b53
Крис <kriska_555@mail.ru>
2023-10-12 20:34:23 +0500

    12/10

--- src/stores/ProductStore.js
+++ src/stores/ProductStore.js
@@ -1,64 +1,68 @@
-import {observable, action, autorun, set, makeObservable} from 'mobx';
+import {observable, action, autorun, set, makeObservable, computed} from 'mobx';
 import {status as statusEnum} from '../enums';
 import api from 'api';
 import {groupArray2Object} from '../utils';
 
 class ProductStore {
 
-    @observable status = statusEnum.LOADING;
-    @observable card = {};
-    @observable fields = {};
-    @observable oldCard = {};
+  @observable status = statusEnum.LOADING;
+  @observable card = {};
+  @observable fields = {};
+  @observable oldCard = {};
 
-    constructor(RouterStore) {
-      this.RouterStore = RouterStore;
+  constructor(RouterStore) {
+    this.RouterStore = RouterStore;
 
-      makeObservable(this);
+    makeObservable(this);
 
-      this.getFieldsDisposer = autorun(this.getFields);
-    }
+    this.getFieldsDisposer = autorun(this.getFields);
+  }
 
-    get baseFields() {
-      return {};
-    }
+  get baseFields() {
+    return {};
+  }
 
-    @action setValue = (name, value) => {
-      set(this.card, {[name]: value});
-    };
+  @computed get category() {
+    return this.card.categoryId || Number(this.RouterStore.getKey('category')) || 1;
+  }
 
-    @action setFields = (fields) => {
-      this.fields = fields;
-    };
+  @action setValue = (name, value) => {
+    set(this.card, {[name]: value});
+  };
 
-    getFields = async() => {
-      const {category} = this;
+  @action setFields = (fields) => {
+    this.fields = fields;
+  };
 
-      try {
-        const fields = await api.post('getFields', {category: category?.value || category});
-        const grouped = groupArray2Object(fields, 'group');
+  getFields = async() => {
+    const {category} = this;
 
-        Object.keys(grouped).forEach((key) => {
-          grouped[key] = grouped[key].sort((a, b) => {
-            if (!!a.isRequired === !!b.isRequired) {
-              const textA = a.title?.toUpperCase();
-              const textB = b.title?.toUpperCase();
+    try {
+      const fields = await api.post('getFields', {category: category?.value || category});
+      const grouped = groupArray2Object(fields, 'group');
 
-              return textA < textB ? -1 : textA > textB ? 1 : 0;
-            }
+      Object.keys(grouped).forEach((key) => {
+        grouped[key] = grouped[key].sort((a, b) => {
+          if (!!a.isRequired === !!b.isRequired) {
+            const textA = a.title?.toUpperCase();
+            const textB = b.title?.toUpperCase();
 
-            return !!a.isRequired < !!b.isRequired ? 1 : !!a.isRequired > !!b.isRequired ? -1 : 0;
-          });
+            return textA < textB ? -1 : textA > textB ? 1 : 0;
+          }
+
+          return !!a.isRequired < !!b.isRequired ? 1 : !!a.isRequired > !!b.isRequired ? -1 : 0;
         });
+      });
 
-        this.setFields(grouped);
-      } catch(err) {
-        console.log(err);
-      }
-    };
-
-    closeStore() {
-      this.getFieldsDisposer();
+      this.setFields(grouped);
+    } catch(err) {
+      console.log(err);
     }
+  };
+
+  closeStore() {
+    this.getFieldsDisposer();
+  }
 }
 
 export default ProductStore;
