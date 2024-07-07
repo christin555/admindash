ead92c15311b0b4191c659a874daa0dd27737b7d
kris <kriska_555@mail.ru>
2024-07-07 14:56:36 +0500

    изменение логики склада

--- src/components/stocks/awaitShipBlock.js
+++ src/components/stocks/awaitShipBlock.js
@@ -12,11 +12,11 @@ const formatBlock = ({row}) => {
 
   const allAmount = awaits.reduce((acc, {amount}) => acc + amount, 0);
 
-  const blocks = awaits.map(({id, shippingDate, amount}) => (
+  const blocks = awaits.map(({id, shippingDate, amount, arrivalId}) => (
     <div key={`${shippingDate}_${amount}`}>
       <span className={s.dateArrival}>{`до ${dayjs(shippingDate).format('DD.MM.YYYY')}`}</span>
       {`, ${amount} уп.`}
-      <span className={s.helperText}>  {`(продажа #${id})`}</span>
+      <span className={s.helperText}>  {`(продажа #${id}${arrivalId ? ' с прихода' : ''})`}</span>
     </div>
   ));
 
