376b57f51ca49f5196bdf65050f7d765a64de3a4
kris <kriska_555@mail.ru>
2024-07-15 20:04:21 +0500

    изменение логики склада

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://localhost:8800/auth/",
-    responseType: "json"
+  baseURL: 'https://api.master-pola.com/auth/',
+  responseType: 'json'
 });
